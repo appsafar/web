42c397b44fea890a8785ffb3943bdc83cec340b8
itsjesusmacias <jesus.macias@cobee.io>
2025-11-04 15:14:48 +0100

    feat: configure montserrat font faml

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -5,6 +5,11 @@ export default function Home() {
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <h1 className="text-4xl font-bold">Hello Safar</h1>
+        <p className="text-lg text-gray-500">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+          quos. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Quisquam, quos. y a e k ñ
+        </p>
         <Button>Click me</Button>
       </main>
     </div>
