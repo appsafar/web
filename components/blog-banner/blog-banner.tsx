9e662018ef06ec6eb2b7bf20e80cbd0bc5b1ec5a
itsjesusmacias <jesus.macias@cobee.io>
2025-11-05 14:51:32 +0100

    feat: update BlogBanner component to use translations for dynamic content; enhance accessibility with localized text for both desktop and mobile views

--- components/blog-banner/blog-banner.tsx
+++ components/blog-banner/blog-banner.tsx
@@ -1,41 +1,35 @@
+import { getTranslations } from "next-intl/server";
 import { Button } from "../ui/button";
 
-const BlogBanner = () => {
+const BlogBanner = async () => {
+  const t = await getTranslations("blogBanner");
+
   return (
     <section className="bg-primary py-10">
       <div className="px-4 md:px-0 md:w-[850px] mx-auto text-white">
         <div className="flex flex-row justify-between md:items-center md:mb-8 mb-4">
-          <h3 className="text-2xl font-bold">
-            Safar nace de una necesidad real
-          </h3>
+          <h3 className="text-2xl font-bold">{t("title")}</h3>
           <Button
             variant="link"
             className="hidden md:block bg-white text-black rounded-3xl hover:bg-accent hover:text-accent-foreground cursor-pointer"
           >
-            Leer más
+            {t("readMore")}
           </Button>
         </div>
         <p className="hidden md:block font-semibold text-lg">
-          Durante mucho tiempo quise viajar pero no quiería hacerlo solo. Busqué
-          comunidades y plataformas, pero ninguna unía compatibilidad, claridad
-          y organización en un mismo lugar. Por eso creé Safar: un espacio donde
-          personas afines se encuentran y convierten ideas en viajes reales. Con
-          grupos privados, chat y herramientas simples, planificar deja de ser
-          un caos y empezar se vuelve fácil.
+          {t("desktop.description")}
         </p>
         <p className="block md:hidden font-medium text-regular">
-          Quería viajar, pero no solo. Probé comunidades y apps: faltaba
-          compatibilidad, claridad y orden.
+          {t("mobile.descriptionPart1")}
         </p>
         <p className="block md:hidden font-medium text-regular mt-2">
-          Por eso creé Safar: conecta a personas afines y convierte ideas en
-          viajes reales con grupos privados, chat y herramientas simples.
+          {t("mobile.descriptionPart2")}
         </p>
         <Button
           variant="link"
           className="block md:hidden mt-4 w-full bg-white text-black rounded-3xl hover:bg-accent hover:text-accent-foreground cursor-pointer"
         >
-          Leer más
+          {t("readMore")}
         </Button>
       </div>
     </section>
